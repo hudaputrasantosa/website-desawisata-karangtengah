985880ee3343e8058992bdf9e93cd32446a6008d
Huda Putra Santosa <hudaputrasantosa@gmail.com>
2022-05-08 23:27:12 +0700

    add berita desa

---
+++ berita-desa.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Berita Desa</title>
+
+   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+   <!-- Vendor CSS Files -->
+   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+   <link href="assets/css/style.css" rel="stylesheet">
+   <link href="assets/css/carousel.css" rel="stylesheet">
+   <link rel="stylesheet" href="assets/css/mdb.min.css" />
+   <link rel="stylesheet" href="assets/css/style.css" />
+
+</head>
+
+<body>
+
+</body>
+
+</html>
