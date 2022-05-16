67880b879a1a8e38a3c20a366d98d418a2692892
Huda Putra Santosa <hudaputrasantosa@gmail.com>
2022-05-16 23:40:08 +0700

    update layout, footer

--- app/Views/berita-desa.php
+++ app/Views/berita-desa.php
@@ -59,7 +59,7 @@
                      <div class="col-md-10 mb-4">
                         <!--Section: Content-->
                         <section>
-                           <h3>Berita Desa</h3><br>
+                           <h2 style="font-weight: bold;">Berita Desa</h2><br>
                            <!-- Post -->
                            <div class="row">
                               <div class="col-md-5 mb-5">
@@ -72,12 +72,12 @@
                               </div>
 
                               <div class="col-md-7 mb-4">
-                                 <h5>Kemitraan Mahasiswa ITTP dengan Desa Karangtengah</h5>
+                                 <h5 style="font-weight: bold;">Kemitraan Mahasiswa ITTP dengan Desa Karangtengah</h5>
                                  <p>
-                                 Sebanyak 4 mahasiswa prodi S1 Software Engineering Melaksanakan Kegiatan Kemitraan
-                          Dengan desa wisata Karangtengah Kab Cilongok. Kemitraan ini untuk memenuhi project 
-                          tugas akhir, dengan mengembangkan sebuah perangkat lunak yaitu website desa wisata
-                          sebagai media informasi dan promosi. 
+                                    Sebanyak 4 mahasiswa prodi S1 Software Engineering Melaksanakan Kegiatan Kemitraan
+                                    Dengan desa wisata Karangtengah Kab Cilongok. Kemitraan ini untuk memenuhi project
+                                    tugas akhir, dengan mengembangkan sebuah perangkat lunak yaitu website desa wisata
+                                    sebagai media informasi dan promosi.
                                  </p>
 
                                  <button type="button" class="btn-hijau">Lihat Selengkapnya</button>
@@ -95,7 +95,7 @@
                               </div>
 
                               <div class="col-md-7 mb-4">
-                                 <h5>Bukit Manggala Karang Tengah Cilongok Ramai Dikunjungi 400 Pengunjung Pada Sabtu dan Minggu</h5>
+                                 <h5 style="font-weight: bold;">Bukit Manggala Karang Tengah Cilongok Ramai Dikunjungi 400 Pengunjung Pada Sabtu dan Minggu</h5>
                                  <p>
                                     Wisata Bukit Manggala ini merupakan wisata dengan pemandangan hamparan bukit yang hijau
                                     yang dianggap seperti diluar negeri yaitu Selandia Baru. Selain disuguhi panorama pemandangan yang hijau
@@ -117,12 +117,12 @@
                               </div>
 
                               <div class="col-md-7 mb-4">
-                                 <h5>Pengusaha Ikan Dewa di Karang Tengah Cilongok Berhenti Produksi Gara-gara Air Sungai Keruh
+                                 <h5 style="font-weight: bold;">Pengusaha Ikan Dewa di Karang Tengah Cilongok Berhenti Produksi Gara-gara Air Sungai Keruh
 
                                  </h5>
                                  <p>
-                                 Pemilik kolam ikan Dewa di desa Karang Tengah Kecamatan Cilongok Banyumas mengeluhkan air sungai Prukut yang kembali keruh akhir-akhir ini.
-                                 Padahal, sungai itu jadi pemasok utama kebutuhan air kolam ikan berkelas itu.
+                                    Pemilik kolam ikan Dewa di desa Karang Tengah Kecamatan Cilongok Banyumas mengeluhkan air sungai Prukut yang kembali keruh akhir-akhir ini.
+                                    Padahal, sungai itu jadi pemasok utama kebutuhan air kolam ikan berkelas itu.
                                  </p>
 
                                  <button type="button" class="btn-hijau">Lihat Selengkapnya</button>
@@ -161,60 +161,55 @@
       </div>
    </main>
 
-    <!-- ======= Bagian Footer ======= -->
-    <footer class="footer">
+   <!-- ======= Bagian Footer ======= -->
+   <footer class="footer">
+      <div class="footer-top">
+         <div class="container">
+            <div class="row gy-4">
+               <div class="col-lg-3 col-md-12 footer-info">
+                  <div class="logo d-flex align-items-center">
+                     <img src="/website-desawisata-karangtengah/public/assets/img/desa-karangtengah-3.png" alt="">
+                  </div>
+                  <p>Desa KarangTengah adalah desa di Kecamatan Cilongok, Banyumas, Jawa Tengah, yang terletak kurang lebih 15 Kilometer ke arah barat dari Purwokerto.
+                  </p>
+               </div>
 
-<div class="footer-top">
-   <div class="container">
-      <div class="row gy-4">
-         <div class="col-lg-5 col-md-12 footer-info">
-            <a href="index.html" class="logo d-flex align-items-center">
-               <img src="assets/img/karangtengah.png" alt="">
+               <div class="col-lg-4 col-md-12 footer-links">
+                  <h4>Kontak Kami</h4>
+                  <p>
+                     Desa Karangtengah
+                     Kecamatan Cilongok<br>
+                     Kabupaten Banyumas, Jawa Tengah<br>
+                     <strong>Phone</strong>: 081287756542 <br>
+                     <strong>Email</strong>: karangtengahdesawisata@gmail.com<br>
+                  </p>
+                  <div class="social-links mt-3">
+                     <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                     <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                     <a href="https://www.instagram.com/desawisata_karangtengah/" class="instagram"><i class="bi bi-instagram"></i></a>
+                  </div>
+               </div>
 
-            </a>
-            <p>Desa KarangTengah adalah desa di Kecamatan Cilongok, Banyumas, Jawa Tengah, yang terletak kurang lebih 15 Kilometer ke arah barat dari Purwokerto.
-               Desa KarangTengah memiliki 7 dusun atau RW, yaitu Karang Tengan, Lebaksiu, Karanganyar, Cirongge, Menggala, Karang Nangka, Semingkir yang memiliki luas wilayah 9.3427 km², dengan jumlah penduduk sebanyak 9375 jiwa.
-               Desa ini memiliki kawasan wisata yang cukup terkenal yaitu wanawisata Curug Cipendok, dengan ketinggian curug 92 meter, langsung dibawah kawasan hutan Gunung Slamet.
-            </p>
-            <div class="social-links mt-3">
-               <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-               <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-               <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+               <div class="col-lg-5 col-md-12 footer-links">
+                  <h4>Lokasi Google Maps Desa</h4>
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63315.911774581575!2d109.12677444999998!3d-7.326413549999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f8a4e4942c393%3A0xfb1656ebb45cb522!2sKarangtengah%2C%20Cilongok%2C%20Banyumas%20Regency%2C%20Central%20Java!5e0!3m2!1sen!2sid!4v1651820938369!5m2!1sen!2sid" width="300" height="200" style="border:0;" allowfullscreen="" loading="fast" referrerpolicy="no-referrer-when-downgrade"></iframe>
+               </div>
 
             </div>
          </div>
-
-         <div class="col-lg-2 col-6 footer-links">
-            <h4>Kontak Kami</h4>
-            <p>
-               Desa Karang Tengah <br>
-               Kecamatan Cilongok<br>
-               Kabupaten Banyumas, Jawa Tengah<br><br>
-               <strong>Phone: 081287756542</strong> <br>
-               <strong>Email:</strong>karangtengahdesawisata@gmail.com<br>
-            </p>
-         </div>
-
-         <div class="col-lg-5 col-md-12 footer-links">
-            <h4>Lokasi Google Maps Desa</h4>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63315.911774581575!2d109.12677444999998!3d-7.326413549999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f8a4e4942c393%3A0xfb1656ebb45cb522!2sKarangtengah%2C%20Cilongok%2C%20Banyumas%20Regency%2C%20Central%20Java!5e0!3m2!1sen!2sid!4v1651820938369!5m2!1sen!2sid" width="300" height="200" style="border:0;" allowfullscreen="" loading="fast" referrerpolicy="no-referrer-when-downgrade"></iframe>
-         </div>
-
       </div>
-   </div>
-</div>
 
-<div class="container">
-   <div class="copyright">
-      &copy; Copyright <strong><span>Pemerintah Desa Karangtengah</span></strong>. All Rights Reserved
-   </div>
-</div>
-</footer><!-- End Footer -->
+      <div class="container">
+         <div class="copyright">
+            &copy; Copyright <strong><span>Pemerintah Desa Karangtengah</span></strong>. All Rights Reserved
+         </div>
+      </div>
+   </footer><!-- End Footer -->
 
    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-   <script src="/desain-pemrograman-web/website-desawisata-karangtengah/public/js/main.js"></script>
-   <script type="text/javascript" src="/desain-pemrograman-web/website-desawisata-karangtengah/public/js/mdb.min.js"></script>
-   <script type="text/javascript" src="/desain-pemrograman-web/website-desawisata-karangtengah/public/js/script.js"></script>
+   <script src="/website-desawisata-karangtengah/public/js/main.js"></script>
+   <script type="text/javascript" src="/website-desawisata-karangtengah/public/js/mdb.min.js"></script>
+   <script type="text/javascript" src="/website-desawisata-karangtengah/public/js/script.js"></script>
 </body>
 
 </html>
