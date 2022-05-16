da89584c5d367d18230228953748790d3a36ddd4
Huda Putra Santosa <hudaputrasantosa@gmail.com>
2022-05-16 23:38:13 +0700

    routing error pages

--- app/Core/Route.php
+++ app/Core/Route.php
@@ -8,10 +8,13 @@ class Route
    public function __construct()
    {
       if (isset($_GET['url'])) {
-         $url = explode('/', filter_var(trim($_GET['url'])), FILTER_SANITIZE_URL);
+         $url = explode('/', filter_var(trim($_GET['url']), FILTER_SANITIZE_URL));
+         $url[0] = $url[0] . 'Controller';
+      } else {
+         $url[0] = 'home';
       }
 
-      $url[0] = $url[0] . 'Controller';
+
 
       if (file_exists('../app/Controllers/' . $url[0] . '.php')) {
          $this->controller = $url[0];
@@ -24,8 +27,11 @@ class Route
       if (isset($url[1])) {
          if (method_exists($this->controller, $url[1])) {
             $this->method = $url[1];
+         } else {
+            return require_once('../app/Views/Errors/404.php');
          }
       }
+
       unset($url[0]);
       unset($url[1]);
       $this->parameter = $url;
