03267af6837e8360853662c4c474955d1b37caa6
Huda Putra Santosa <hudaputrasantosa@gmail.com>
2022-07-19 23:57:45 +0700

    cretae sub-page admin

---
+++ admin/navigationCollapse.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login'])) {
+   header("location: ./index.php");
+   exit;
+}
+include 'config.php';
+?>
+<nav class="navbar navbar-default navbar-static-top" style="margin-bottom: 0">
+   <div class="navbar-header"> <a class="navbar-toggle hidden-sm hidden-md hidden-lg " href="javascript:void(0)" data-toggle="collapse" data-target=".navbar-collapse"><i class="ti-menu"></i></a>
+      <div class="top-left-part"><a class="logo" href="dashboard.html"><i class="glyphicon glyphicon-fire"></i>&nbsp;<span class="hidden-xs">My Admin</span></a></div>
+      <ul class="nav navbar-top-links navbar-left hidden-xs">
+         <li><a href="javascript:void(0)" class="open-close hidden-xs hidden-lg
+ waves-effect waves-light"><i class="ti-arrow-circle-left ti-menu"></i>
+            </a></li>
+      </ul>
+      <ul class="nav navbar-top-links navbar-right pull-right">
+
+         <li>
+            <a class="profile-pic" href="#"> <img src="images/users/hritik.jpg" alt="user-img" width="36" class="img-circle"><b class="hidden-xs">Admin</b> </a>
+         </li>
+      </ul>
+   </div>
+
+</nav>
+<div class="navbar-default sidebar nicescroll" role="navigation">
+   <div class="sidebar-nav navbar-collapse ">
+      <ul class="nav" id="side-menu">
+         <li class="sidebar-search hidden-sm hidden-md hidden-lg">
+            <div class="input-group custom-search-form">
+               <input type="text" class="form-control" placeholder="Search...">
+               <span class="input-group-btn">
+                  <button class="btn btn-default" type="button"><i class="ti-search"></i> </button>
+               </span>
+            </div>
+         </li>
+         <li>
+            <a href="./dashboard.php" class="waves-effect">
+               Profil Desa</a>
+         </li>
+         <li>
+            <a href="./berita-admin" class="waves-effect">Berita Desa</a>
+         </li>
+         <li>
+            <a href="./wisata-admin" class="waves-effect">Wisata Desa</a>
+         </li>
+         <li>
+            <a href="./produk-admin" class="waves-effect">Produk Desa</a>
+         </li>
+         <li>
+            <a href="./footer-admin" class="waves-effect">Footer</a>
+         </li>
+
+      </ul>
+      <div class="center p-20">
+         <span class="hide-menu"><a href="http://wrappixel.com/templates/myadmin/" target="_blank" class="btn btn-danger btn-block btn-rounded waves-effect waves-light">Keluar</a></span>
+      </div>
+   </div>
+   <!-- /.sidebar-collapse -->
+</div>
