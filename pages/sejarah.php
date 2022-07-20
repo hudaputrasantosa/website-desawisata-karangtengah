6818a9e70b87bdaf09dea7c4662f557e542ef812
Fajar Maulana Thaariq A <75149615+alburuuj30@users.noreply.github.com>
2022-07-21 02:32:10 +0700

    update page user

--- pages/sejarah.php
+++ pages/sejarah.php
@@ -26,7 +26,7 @@
       <div class="container col-xxl-8 px-4 py-5">
          <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
             <div class="col-10 col-sm-8 col-lg-6">
-               <img src="../assets/img/banner/<?= $row['foto']; ?>" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+               <img src="../assets/img/sejarah/<?= $row['foto']; ?>" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
             </div>
             <div class="col-lg-6">
                <h1 class="display-5 fw-bold mt-5 lh-1 mb-4">Sejarah Desa Karangtengah</h1>
